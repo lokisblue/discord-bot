8d4e529061499069c1796d5a929c195640812d72
lokisblue <serbent1221@gmail.com>
2025-10-21 01:40:34 +0300

    tokens.js

--- tokens.js
+++ tokens.js
@@ -16,3 +16,18 @@ export default [
         selfMute: true,
     },
 ];
+   {
+        channelId: "1185756128542478366",
+        serverId: "1133667699151618130",
+        token: process.env.token2,
+        selfDeaf: false,
+        autoReconnect: {
+            enabled: true,
+            delay: 5, // ثواني
+            maxRetries: 5,
+        },
+        presence: {
+            status: "idle",
+        },
+        selfMute: true,
+    },
